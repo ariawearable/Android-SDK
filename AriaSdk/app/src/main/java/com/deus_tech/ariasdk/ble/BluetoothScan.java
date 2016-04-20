a194c6d082dab5e8d2cb49c05f572f0377cdb1e5
Anton Pinchuk <antonpinchuk@yandex.ru>
2016-04-20 20:57:23 +0300

    BLE scan using BluetoothLeScanner, request location permissions

--- AriaSdk/app/src/main/java/com/deus_tech/ariasdk/ble/BluetoothScan.java
+++ AriaSdk/app/src/main/java/com/deus_tech/ariasdk/ble/BluetoothScan.java
@@ -1,14 +1,23 @@
 package com.deus_tech.ariasdk.ble;
 
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class BluetoothScan {
 
 	private static final String TAG = "TAG";
@@ -22,6 +31,10 @@ public class BluetoothScan {
 	private DiscoveryListener mDiscoveryListener;
 
 	private BluetoothAdapter btAdapter;
+
+	private BluetoothLeScanner mLEScanner;
+	private ScanSettings mLESettings;
+	private ArrayList<ScanFilter> mLEFilters;
 
 	public interface DiscoveryListener {
 		void onDeviceFound(BluetoothDevice device);
@@ -37,22 +50,27 @@ public class BluetoothScan {
 	public void startLeScan(@NonNull DiscoveryListener listener) {
 		Log.d("", "in btDiscoverDevices");
 
-		mDiscoveryListener = null;
-		stopLeScan();
-		mDiscoveryListener = listener;
-
 		mDeviceArray.clear();
 
 		if (checkDeviceForBtSupport()) {
-			if (btAdapter.startLeScan(mLeScanCallback)) {
+			//if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				mLEScanner = btAdapter.getBluetoothLeScanner();
+					mLESettings = new ScanSettings.Builder()
+							.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+							//.setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+							.build();
+				mLEFilters = new ArrayList<>();
+
+				mDiscoveryListener = null;
+				stopLeScan();
+				mDiscoveryListener = listener;
+
+				mLEScanner.startScan(mLEFilters, mLESettings, mLeScanCallback);
 				_discoveryLeHandler.removeCallbacks(_discoveryLeRunnable);
 				_discoveryLeHandler.postDelayed(_discoveryLeRunnable, LE_SCAN_PERIOD);
 				mDiscoveryListener.onStarted();
 				Log.d(TAG, "LE start discovery ok");
-			} else {
-				mDiscoveryListener.onFinished(null);
-				Log.d(TAG, "LE Discovery failed.");
-				//Discovery failed.
 			}
 		} else {
 			mDiscoveryListener.onFinished(null);
@@ -66,13 +84,14 @@ public class BluetoothScan {
 		_discoveryLeHandler.removeCallbacks(_discoveryLeRunnable);
 		mLeDeviceLastResponseMap.clear();
 		if (checkDeviceForBtSupport()) {
-			btAdapter.stopLeScan(mLeScanCallback);
+			mLEScanner.stopScan(mLeScanCallback);
 		}
 	}
 
-	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+	private ScanCallback mLeScanCallback = new ScanCallback() {
 		@Override
-		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+		public void onScanResult(int callbackType, ScanResult result) {
+			BluetoothDevice device = result.getDevice();
 			String deviceName = device.getName();
 			if (deviceName != null) {
 				if (!mLeDeviceLastResponseMap.containsKey(deviceName)) {
@@ -82,8 +101,24 @@ public class BluetoothScan {
 				}
 				mLeDeviceLastResponseMap.put(deviceName, System.currentTimeMillis());
 			}
+			Log.i("callbackType", String.valueOf(callbackType));
+			Log.i("result", result.toString());
+		}
+
+		@Override
+		public void onBatchScanResults(List<ScanResult> results) {
+			for (ScanResult sr : results) {
+				Log.i("ScanResult - Results", sr.toString());
+			}
+		}
+
+		@Override
+		public void onScanFailed(int errorCode) {
+			Log.e("Scan Failed", "Error Code: " + errorCode);
+			mDiscoveryListener.onFinished(null);
 		}
 	};
+
 
 	private Handler _discoveryLeHandler = new Handler();
 	private Runnable _discoveryLeRunnable = new Runnable() {
